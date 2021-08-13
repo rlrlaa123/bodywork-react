3ea9e3b62fe757094d3b26d71ed53bd7ef9546c1
rlrlaa123 <kimdonghyun3366@gmail.com>
2021-08-14 01:25:58 +0900

    Add post controller (get, post, delete, put, patch) with Koa

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,54 @@
-const hello = 'hello';
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// // 라우터 설정
+// router.get('/', (ctx) => {
+//   ctx.body = '홈';
+// });
+// router.get('/about/:name?', (ctx) => {
+//   // name의 존재 유무에 따라 다른 결과 출력
+//   const { name } = ctx.params;
+//   ctx.body = name ? `${name}의 소개` : '소개';
+// });
+// router.get('/posts', (ctx) => {
+//   const { id } = ctx.query;
+//   // id의 존재 유무에 따라 다른 결과 출력
+//   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+// });
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(async (ctx, next) => {
+//   console.log(ctx.url);
+//   console.log(1);
+//   if (ctx.query.authorized !== '1') {
+//     ctx.status = 401; // Unauthorized
+//     return;
+//   }
+//   await next();
+//   console.log('END');
+//   // next().then(() => {
+//   //   console.log('END');
+//   // })
+// });
+
+// app.use((ctx) => {
+//   ctx.body = 'hello world';
+// });
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
