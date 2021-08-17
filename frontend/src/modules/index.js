db325013affe97d289a8ad354eeb8277b253c1cf
rlrlaa123 <kimdonghyun3366@gmail.com>
2021-08-18 01:23:29 +0900

    Add Axios instance, Proxy, loading and createRequestSage
    Add Axios instance to add common setting for API requests on src/lib/api/client.js
    Proxy is set to bypass CORS instaed of giving bypass setting to server
    Redux Saga is applied with Loading and createReduxSage modules it
    handles all API requests with loading, success, failure

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
+import loading from "./loading";
 import write from "./write";
 import alliance from "./alliance";
 
 const rootReducer = combineReducers({
+  loading,
   alliance,
   write,
 });
