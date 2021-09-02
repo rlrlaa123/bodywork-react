dec3d8f43a518ef1ca0c935490b7a66dade3c920
rlrlaa123 <kimdonghyun3366@gmail.com>
2021-09-02 19:25:51 +0900

    Modify WriteAction Buttons style and name

--- frontend/src/components/Dashboard/write/WriteActionButtons.js
+++ frontend/src/components/Dashboard/write/WriteActionButtons.js
@@ -8,7 +8,7 @@ const WriteActionButtonsBlock = styled.div`
   button + button {
     margin-left: 0.5rem;
   }
-  text-align: left;
+  text-align: right;
 `;
 
 /* TagBox에서 사용하는 버튼과 일치하는 높이로 설정 후 서로 간의 여백 지정 */
@@ -22,7 +22,7 @@ const StyledButton = styled(Button)`
 const WriteActionButtons = ({ onCancel, onPublish }) => {
   return (
     <WriteActionButtonsBlock>
-      <StyledButton onClick={onPublish}>포스트 등록</StyledButton>
+      <StyledButton onClick={onPublish}>작성완료</StyledButton>
       <StyledButton onClick={onCancel}>취소</StyledButton>
     </WriteActionButtonsBlock>
   );
