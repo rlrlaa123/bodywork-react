627f057414818340ed43ae638eacef7017536604
rlrlaa123 <kimdonghyun3366@gmail.com>
2021-09-10 00:20:58 +0900

    Add aos to Carousel1 component
    Added animate on scroll to Carouel but it does not look good on
    carousels
    I will add aos to other home page components

--- frontend/src/components/Carousel1/Carousel1.js
+++ frontend/src/components/Carousel1/Carousel1.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-// import "./Carousel1.css";
+import React, { useEffect, useState } from "react";
 import main1 from "../../assets/main1.png";
 import main2 from "../../assets/main2.jpeg";
 import main3 from "../../assets/main3.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import {
   Carousel,
@@ -53,6 +54,10 @@ const Carousel1 = (props) => {
     setActiveIndex(newIndex);
   };
 
+  useEffect(() => {
+    AOS.init({ duration: 3000 });
+  }, []);
+
   const slides = items.map((item) => {
     return (
       <CarouselItem
@@ -62,7 +67,12 @@ const Carousel1 = (props) => {
         onExited={() => setAnimating(false)}
         key={item.id}
       >
-        <img src={item.src} alt={item.altText} width={"100%"} />
+        <img
+          src={item.src}
+          alt={item.altText}
+          width={"100%"}
+          data-aos="fade-up"
+        />
       </CarouselItem>
     );
   });
@@ -86,25 +96,37 @@ const Carousel1 = (props) => {
             .custom-indicator .active {
               background-color: orange;
             }
+            .custom-carousel {
+              margin: auto;
+              width: 90%;
+            }
           `}
       </style>
-      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+        className="custom-carousel"
+      >
         <CarouselIndicators
           className="custom-indicator"
           items={items}
           activeIndex={activeIndex}
           onClickHandler={goToIndex}
+          data-aos="fade-up"
         />
         {slides}
         <CarouselControl
           direction="prev"
           directionText="Previous"
           onClickHandler={previous}
+          data-aos="fade-up"
         />
         <CarouselControl
           direction="next"
           directionText="Next"
           onClickHandler={next}
+          data-aos="fade-up"
         />
       </Carousel>
     </div>
